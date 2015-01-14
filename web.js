378afefc128c3c5db213c8d701866f902824a158
Brenda Jin <brendajin@gmail.com>
2015-01-14 15:58:03 -0800

    json is being served, but still waiting for mock data for products.json #6

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
-var https = require('https'),
-	  express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    port = process.env.PORT || 2500;
+var https 		= require('https'),
+	express 	= require('express'),
+    app 		= express(),
+    bodyParser 	= require('body-parser'),
+    fs 			= require('fs'),
+    port 		= process.env.PORT || 2500;
 
 // for heroku
 process.env.PWD = process.cwd();
@@ -10,8 +11,12 @@ process.env.PWD = process.cwd();
 app.use(bodyParser.json());
 
 // manage proxy routes
-app.get(['/json/*'], function( req, res ) {
-	res.json( req.body );
+app.get(['/api/*'], function( req, res ) {
+	var filepath = __dirname + req.path,
+		json = fs.readFileSync(filepath, 'utf8');
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send( json );
 });
 
 app.get(['/*', '/#*'], express.static(__dirname + '/src'));
