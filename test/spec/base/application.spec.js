a411becc890edc0b2bc7b75f5085756b71c57ae7
Tiago Garcia <tiagoromerogarcia@gmail.com>
2015-01-26 17:53:59 -0800

    improving the application specs

--- test/spec/base/application.spec.js
+++ test/spec/base/application.spec.js
@@ -1,19 +1,26 @@
-'use strict';
-
 define( [ 'base/application', 'marionette' ], function ( Application, Marionette ) {
+
+    'use strict';
+
     describe( 'Application', function () {
-        beforeEach( function () {
-            loadFixtures( 'application.html' )
-        } );
+        var application;
+
+        loadFixtures( 'application.html' );
 
         it( 'should be a Marionette.Application', function () {
             expect( typeof Application ).toEqual( 'function' );
 
-            var application = new Application();
+            application = new Application();
             expect( application instanceof Marionette.Application ).toEqual( true );
+        } );
 
+        it( 'should have a rootView with regions', function () {
             expect( application.rootView ).toBeDefined();
             expect( application.rootView instanceof Marionette.LayoutView ).toEqual( true );
+
+            expect( Object.keys( application.rootView.regions ) ).toContain( 'header' );
+            expect( Object.keys( application.rootView.regions ) ).toContain( 'sidebar' );
+            expect( Object.keys( application.rootView.regions ) ).toContain( 'content' );
         } );
     } );
 } );
